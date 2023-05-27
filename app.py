cbb964c9f516203f6ac449e4ea26ea207ec13ccf
Michał Nadolny <michalnadolny192@gmai.com>
2023-05-27 14:53:18 +0200

    added threads to calculate the pow

--- app.py
+++ app.py
@@ -6,18 +6,99 @@ from json_utils import (
     create_record_for_block,
     add_block_to_chain,
 )
+import threading
+import time
+from queue import Queue
+import sys
+import random
 
 
-init_blockchain(1)
-block_id, block = create_block(0, "aaa", ["bbb", "ccc"], 2137)
-block = create_record_for_block(block, "P.K. to najlepszy prowadzący")
-block = create_record_for_block(block, "P.K. to najlepszy prowadzący")
-block = create_record_for_block(block, "P.K. to najlepszy prowadzący")
-add_block_to_chain(block, 1)
+input_queue = Queue()
+finished_pow_queue = Queue()
 
-init_blockchain_from_neighbour(2, 1)
-block_id, block = create_block(block_id, "aaa", ["bbb", "ccc"], 2137)
-block = create_record_for_block(block, "M.S. to najlepszy prowadzący")
-block = create_record_for_block(block, "M.S. to najlepszy prowadzący")
-block = create_record_for_block(block, "M.S. to najlepszy prowadzący")
-add_block_to_chain(block, 2)
+class CommunicationThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        init_blockchain(1)
+        self.block_id, self.block = create_block(0, "aaa", ["bbb", "ccc"], 2137)
+
+    def run(self):
+        while True:
+            # Receive a new record from socket tba
+            self.block = create_record_for_block(self.block, "P.K. to najlepszy prowadzący")
+            time.sleep(0.5)
+            self.block = create_record_for_block(self.block, "P.K. to najgorszy prowadzący")
+
+            # sort by timestamp and change indices so that the oldest record is first and the newest last
+            self.block['records'] = sorted(self.block['records'], key=lambda record: record['timestamp'])
+            for i in range(len(self.block['records'])):
+                self.block['records'][i]['index'] = i + 1
+
+            # Send immediately to POW thread
+            input_queue.put(self.block)
+            break
+
+
+class PowThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.running = False
+        self.block = None
+
+    def run(self):
+        # input hold curr block for which the pow is calculated at the moment
+        while True:
+            if not input_queue.empty():
+                print('POW: fethed a new record')
+                self.block = input_queue.get()
+                self.running = False
+
+            if self.running:
+                # Make attempt to calculate the pow
+                time.sleep(1)
+                n = random.random()
+                print('An attempt to calculate pow...')
+                if n < 0.1:
+                    finished_pow_queue.put(self.block)
+                    self.block = None
+                    self.running = False
+                    # Replace curr block pow field with calculated pow
+
+            elif not self.running and self.block != None:
+                # Check if there isnt another pow to calculate
+                self.running = True
+
+
+class IoThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        i = 1
+        while True:
+            if not finished_pow_queue.empty():
+                block = finished_pow_queue.get()
+                print('Calculated pow for this block:')
+                print(block)
+                add_block_to_chain(block, 1)
+
+
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.comm_thread = CommunicationThread()
+        self.io_thread = IoThread()
+        self.pow_thread = PowThread()
+
+    def run(self):
+        self.comm_thread.start()
+        self.io_thread.start()
+        self.pow_thread.start()
+
+        self.comm_thread.join()
+        self.io_thread.join()
+        self.pow_thread.join()
+
+
+node = Node(0)
+node.run()
